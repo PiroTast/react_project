aa7443ecbbed55ba66db9de630d999157b9f7363
Sergey <serejinhtc@gmail.com>
2024-12-05 23:04:53 +0300

    another 10 minutes of coding (better then 0 minutes, but still not enough)

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function App() {
-  let likes = 0;
+  const [likes, setLikes] = useState (0)
+  const [value, setValue] = useState ('ТЕКСТ В ИНПУТЕ')
+
+
+
   return (
     <div className="App">
       <h1>{likes}</h1>
-      <button onClick={() => likes += 1}>Увеличить</button>
-      <button onClick={() => likes -= 1}>Уменьшить</button>
+      <h1>{value}</h1>
+      <input 
+        type="text" 
+        value = {value}
+        onChange={event => setValue(event.target.value)}
+      />
+      <button onClick={increment}>Увеличить</button>
+      <button onClick={decrement}>Уменьшить</button>
     </div>
   );
 }
---
+++ react_project/src/components/Counter.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const Counter = function () {
+    const [cont, setCount] = useState (0)
+
+    function increment () {
+        setLikes(likes + 1)
+      }
+    
+      function decrement () {
+        setLikes(likes - 1)
+      }
+
+    return {
+        
+        
+       
+    }
+}
+
+export default Counter;
